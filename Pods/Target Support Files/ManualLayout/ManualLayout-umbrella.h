3bc0daaf4ce72c9fd3628a25a90230869bf6da40
Łukasz Śliwiński <lsliwinski@pgs-soft.com>
2016-12-22 11:11:28 +0100

    Removed Pods

--- Pods/Target Support Files/ManualLayout/ManualLayout-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "ManualLayout.h"
-
-FOUNDATION_EXPORT double ManualLayoutVersionNumber;
-FOUNDATION_EXPORT const unsigned char ManualLayoutVersionString[];
-
--- Pods/Target Support Files/Pods-TVOSToastTests/Pods-TVOSToastTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TVOSToastTests : NSObject
-@end
-@implementation PodsDummy_Pods_TVOSToastTests
-@end
